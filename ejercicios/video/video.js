9f0ead75daa95afca2780487987259b0b12792c9
msmonje <mariana.sueiro@gmail.com>
2021-06-17 09:22:13 +0200

    Update video.js

--- ejercicios/video/video.js
+++ ejercicios/video/video.js
@@ -1,19 +1,7 @@
 
-
-    function draw() {
-        var canvas = document.getElementById('canvas2');
-        if (canvas.getContext) {
-          var ctx = canvas.getContext('2d');
-      
-          // Cubic curves example
-          ctx.beginPath();
-          ctx.moveTo(75, 40);
-          ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
-          ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
-          ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
-          ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
-          ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
-          ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
-          ctx.fill();
-        }
-      }
+  
+    var c = document.getElementById("canvas");
+    var ct = c.getContext('2d');
+    ct.font = "30px Arial";
+    ct.strokeText("Esto es un canvas",10,50);
+  
